b4fa1034b1cd6dfaababcc7ad9faeeddb756720a
arutyunyan <arutyunyan@bolid.ru>
2024-04-12 15:59:14 +0300

    refactor

--- src/Js.php
+++ src/Js.php
@@ -1,23 +1,23 @@
 <?php
+    ob_start();
+
     $scope = [
         'links' => $this->links,
         'vars' => $this->vars,
         'set' => require 'js/scope.set.php',
     ];
+
+    echo 'const ' . $this->getScopeName() . ' = ' . json_encode($scope);
+    echo file_get_contents($this->js());
+
+    // TODO: Build scope of the nested components here
+
+    if ($this->isGlobal()) {
+        echo 'var global = global || window';
+        echo "global.{$this->getScopeName()} = {$this->getScopeName()};";
+    }
+
+    $jsScript = ob_get_clean();
+
+    echo $this->useScope() ? '(function(global){' . $jsScript . '})(window);' : $jsScript;
 ?>
-
-<script type="text/javascript">
-    <?=$this->useScope() ? '(function(global){' : ''?>
-
-        const <?=$this->getScopeName()?> = <?=json_encode($scope)?>;
-
-        <?=file_get_contents($this->js())?>;
-
-        <?php
-            // TODO: Build scope of the nested components
-        ?>
-
-        <?=$this->isGlobal() ? "global = global || window; global.{$this->getScopeName()} = {$this->getScopeName()};" : ''?>
-
-    <?=$this->useScope() ? '})(window);' : ''?>
-</script>
